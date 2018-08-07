aba13671d0d2058143b16dafc32878e7bd6b662f
Tamsen1995 <Tamanhbui95@gmail.com>
2018-08-07 19:51:32 +0200

    [UPD] There is a datasnapshot I can't handle

--- MusicRoom/Views and Controllers/UserArea/ProfileScreenViewController.swift
+++ MusicRoom/Views and Controllers/UserArea/ProfileScreenViewController.swift
@@ -7,22 +7,42 @@
 //
 
 import UIKit
+import Firebase
 
 class ProfileScreenViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayedEmail: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        getEmailIntoProfile()
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    func getEmailIntoProfile() {
+        guard let uid = Auth.auth().currentUser?.uid else {
+            fatalError("\nUser id could not be retrieved on the ProfileScreenViewController\n")
+        }
+        FirebaseManage.shared.retrieveUserInfo(uid: uid, { (snapshot) in
+            if !snapshot.exists() { return }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            for elem in snapshot.children {
+                print(elem) // TESTING
+                // Need to figure out how to handle the data snapshot
+            }
+            
+//            print(snapshot.children)
+//                if let email = snapshot.ch as? String {
+//                    print("\nEmail is good\n", email)
+//                }
+            
+        })
     }
     
     
-
     
-
+    
+    
 }
